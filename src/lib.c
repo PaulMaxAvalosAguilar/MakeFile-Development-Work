1cd3360895217432df257327d1961e816e9976b7
Paul Max Avalos Aguilar <pauldromeasaurio@hotmail.com>
2018-01-21 19:57:34 -0600

    Added files

---
+++ src/lib.c
@@ -0,0 +1,6 @@
+#include "lib.h"
+#include <stdio.h>
+
+void hello(){
+	printf("My name is Paul and this is working\n");
+}
---
+++ src/project.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lib.h"
+#include "a.h"
+
+
+int main(){
+	
+	hello();
+	printf("%s", "hello to everyone u fools\n");
+	goodbye();
+	
+
+	
+	return 0;
+}
