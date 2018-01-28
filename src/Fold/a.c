b2b23b270c15094db87b6c2d07c7291a70bb629b
Paul Max Avalos Aguilar <pauldromeasaurio@hotmail.com>
2018-01-27 22:54:59 -0600

    deleted unuseful files

--- src/Fold/a.c
+++
@@ -1,6 +0,0 @@
-#include "a.h"
-#include <stdio.h>
-
-void goodbye(){
-	printf("I am sorry but I will have to say goobye to you\n");
-}
--- src/lib.c
+++
@@ -1,6 +0,0 @@
-#include "lib.h"
-#include <stdio.h>
-
-void hello(){
-	printf("My name is Paul and this is working\n");
-}
