1d14be6cfb68af170b6c219cace1070528f02693
Paul Max Avalos Aguilar <pauldromeasaurio@hotmail.com>
2018-02-21 20:14:07 -0600

    Sharedlibraries project update

---
+++ Src_Modules/SharedLibTest/src/Dylib.c
@@ -0,0 +1,6 @@
+#include "Dylib.h"
+#include <stdio.h>
+
+void dylib(){
+  printf("This is a message from a dynamic lib\n");
+}
---
+++ Src_Modules/SharedLibTest/src/Dylib.h
@@ -0,0 +1,6 @@
+#ifndef Dylib_h
+#define Dylib_h
+
+void dylib();
+
+#endif
